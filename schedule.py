e62cf853e30b7cd6aca4484bc576ada61b91d720
phphe <phphe@outlook.com>
2018-03-20 17:58:38 +0800

    fix msg datetime; short link(m.me)

--- schedule.py
+++ schedule.py
@@ -1,6 +1,6 @@
 from utils import getGoogleSheetService, getGoogleCalendarService, sendSms
 from datetime import datetime, timedelta
-from config import SPREADSHEETID, fb_PAGE_ACCESS_TOKEN, schedule_delay
+from config import SPREADSHEETID, fb_PAGE_ACCESS_TOKEN, schedule_delay, fb_PAGE_NAME
 import requests
 import time
 import urllib.parse
@@ -29,27 +29,35 @@ while True:
         spreadsheetId=spreadsheetId, range=rangeName).execute()
     sheetRows = result.get('values', [])
     def findRow(phone, name):
-        return next(row for row in sheetRows if row[0] == phone and row[1] == name)
+        try:
+            return next(row for row in sheetRows if row[0] == phone and row[1] == name)
+        except StopIteration as e:
+            return None
 
     for event in events:
         ls = event['summary'].split(' ')
         phone = ls[0]
         name = ls[1]
         row = findRow(phone, name)
+        if not row:
+            print('row not found for event: %s'%(event['summary']))
+            continue
         print(row)
         phone = row[0]
         facebookid = row[3]
-        bookingDatetime = '%s %s:%s'%(row[4], row[5], row[6])
+        datetime.strptime(event['start']['dateTime'][:19], "%Y-%m-%dT%H:%M:%S")
+        bookingDatetime = datetime.strftime('%Y-%m-%dT%H:%M:%S')
         msgFront = 'Hi %s, Your booking is %s.'%(row[1], bookingDatetime)
         if phone:
             # send sms
             params = {'phone': phone}
             if facebookid:
                 params['fbid'] = facebookid
-            link = 'https://www.messenger.com/t/498812477183171?phone=%s'%(urllib.parse.urlencode(params))
+            # link = 'https://www.messenger.com/t/498812477183171?%s'%(urllib.parse.urlencode(params))
+            link = 'http://m.me/%s?%s'%(fb_PAGE_NAME, urllib.parse.urlencode(params))
             msg = msgFront + ' For more info pleaes check out our chatbot %s.'%(link)
             sendSms(phone, msg)
-            print('sms sent')
+            print('sms sent', msg)
         if facebookid:
             # send fb msg
             msgDict = {
@@ -61,6 +69,6 @@ while True:
                     "text": msgFront,
                 }
             }
-            r = requests.post('https://graph.facebook.com/v2.6/me/messages?access_token=%s'%(fb_PAGE_ACCESS_TOKEN), data=msgDict)
+            r = requests.post('https://graph.facebook.com/v2.6/me/messages?access_token=%s'%(fb_PAGE_ACCESS_TOKEN), json=msgDict)
             print('fb message sent', r.text)
     time.sleep(schedule_delay)
