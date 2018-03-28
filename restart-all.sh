54b94827d188525d92a76e4436cced5f042d92c5
phphe <phphe@outlook.com>
2018-03-28 10:23:02 +0800

    fix tornado-sever start error

--- restart-all.sh
+++ restart-all.sh
@@ -6,5 +6,8 @@ sudo unlink /var/run/supervisor.sock
 sudo supervisord -c /etc/supervisor/supervisord.conf
 # start tornado-server(run.py)
 sudo supervisorctl start tornado-server
+echo 'it is right if you get tornado-server: ERROR (spawn error)'
 # start schedule
 sudo supervisorctl start schedule
+echo 'now it will visit https://knode.co:8083/ to check if app is running. Make sure the output is like Hello World!'
+curl https://knode.co:8083/
